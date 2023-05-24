65e1aa96120c8d918ffc7d8f589a18e436b6b979
IGamesI <69199901+IGamesI@users.noreply.github.com>
2023-05-24 12:42:21 +0330

    Player + Tree Placement Improvement

--- Assets/Scripts/FloorObjectsHandler.cs
+++ Assets/Scripts/FloorObjectsHandler.cs
@@ -7,26 +7,64 @@ public class FloorObjectsHandler : MonoBehaviour
 {
     #region Variables
     [SerializeField] GameObject _treePrefab;
+	[SerializeField] float _treeWidth;
+	[SerializeField] float _minTreeWidth;
 
-	Random _treeAmmountRandom = new Random();
+	Random _treeAmountRandom = new Random();
 	Random _treeRandom = new Random();
 	#endregion
 
 	#region UnityFunctions
 	void Start()
     {
-		float treeAmmount = _treeAmmountRandom.Next(0, 10);
-		for (int i = 0; i < treeAmmount; i++)
+		float treeAmount = _treeAmountRandom.Next(0, 8);
+		List<float> treeXPositions = new List<float>();
+		for (int i = 0; i < treeAmount; i++)
 		{
-			print("Added new tree!");
-			float treeRandomX = _treeRandom.Next(-11, 11);
-			Vector3 newTreePosition = new Vector3(gameObject.transform.localPosition.x + treeRandomX, 0, 0);
-			Instantiate(_treePrefab, newTreePosition, _treePrefab.transform.rotation, gameObject.transform);
+			float treeRandomX = 0f;
+			bool tooClose = false;
+			int counter = 0;
+			while (tooClose && counter < 100)
+			{
+				treeRandomX = _treeRandom.Next(-8, 8);
+				if (treeXPositions.Count > 0)
+				{
+					foreach (float x in treeXPositions)
+					{
+						if (Mathf.Abs(treeRandomX - x) <= _minTreeWidth)
+						{
+							tooClose = false;
+						}
+						else if(Mathf.Abs(treeRandomX - x) < _treeWidth)
+						{
+							tooClose = true;
+							break;
+						} 
+						else
+						{
+							tooClose = false;
+						}
+					}
+				}
+				else
+				{
+					tooClose = false;
+				}
+				counter++;
+			}
 
+			if (counter < 100)
+			{
+				treeXPositions.Add(treeRandomX);
+				Vector3 newTreePosition = new Vector3(gameObject.transform.localPosition.x + treeRandomX, 0, 0);
+				Instantiate(_treePrefab, newTreePosition, _treePrefab.transform.rotation, gameObject.transform);
+			}
 		}
-	}
-    #endregion
 
-    #region CustomFunctions
-    #endregion
+
+	}
+	#endregion
+
+	#region CustomFunctions
+	#endregion
 }
---
+++ Assets/Scripts/HumanMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HumanMovement : MonoBehaviour
+{
+	#region Variables
+	float _speed = 0f;
+	[SerializeField] float _jumpForce;
+	[SerializeField] LayerMask _groundLayer;
+	Rigidbody2D _rigidBody;
+	BoxCollider2D _boxCollider;
+	bool _isGrounded;
+	#endregion
+
+	#region UnityFunctions
+	void Start()
+	{
+		_rigidBody = GetComponent<Rigidbody2D>();
+		_boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	void Update()
+	{
+		_rigidBody.velocity = new Vector2(_speed, _rigidBody.velocity.y);
+		_isGrounded = Physics2D.IsTouchingLayers(_boxCollider, _groundLayer);
+		if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded)
+		{
+			_rigidBody.AddForce(new Vector2(0f, _jumpForce), ForceMode2D.Impulse);
+		}
+	}
+	#endregion
+
+	#region CustomFunctions
+	#endregion
+}
